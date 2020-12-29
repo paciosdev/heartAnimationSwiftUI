540fc1d4a9e981155ff1aea7e5c96fc6467c2f19
Stephen DeStefano <sd961960@gmail.com>
2020-12-29 16:59:39 -0500

    Initial Commit

---
+++ Heart Animate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Heart Animate
+//
+//  Created by Stephen DeStefano on 12/29/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Heart Animate/Heart_AnimateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Heart_AnimateApp.swift
+//  Heart Animate
+//
+//  Created by Stephen DeStefano on 12/29/20.
+//
+
+import SwiftUI
+
+@main
+struct Heart_AnimateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
